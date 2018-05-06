357163b744423d0bf9cafacc70b547300057ed25
Gabriel Lafrange <gabriellafrange@gmail.com>
2018-05-06 19:24:26 -0400

    added index.html

--- pomodoro.js
+++ pomodoro.js
@@ -1,21 +1,25 @@
+  //node
+//var args = process.argv.map(arg => parseInt(arg)).splice(2);
+//let timer = new Pomodoro(...args);
+
+const timerElem = document.getElementById('timer');
+let timer = new Pomodoro(5, 10);
+
 function Pomodoro(minutes, seconds=0) {
   
   this.seconds = (minutes*60) + seconds;
   
-  this.timeout = function(seconds) {
+  this.timeout = (seconds) => {
     setTimeout(() => {
       if (seconds === 0) {
         console.log('done');
       } else {
         console.log(parseInt(seconds/60) + ":" + ((seconds%60).toString().length === 1 ? "0" + (seconds%60).toString() : seconds%60));
+        timerElem.innerHTML = parseInt(seconds/60) + ":" + ((seconds%60).toString().length === 1 ? "0" + (seconds%60).toString() : seconds%60);
         this.timeout(seconds - 1);
       }
     }, 1000);
   };
 }
-
-var args = process.argv.map(arg => parseInt(arg)).splice(2);
-
-let timer = new Pomodoro(...args);
 
 timer.timeout(timer.seconds);
